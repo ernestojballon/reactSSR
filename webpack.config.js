40fb65db61fd0f3aae6f810fa8f4e70ea9b0dd3b
Ernesto Ballon <ernestoballono@gmail.com>
2021-03-20 16:58:01 -0400

    adding some spaces to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,32 +14,32 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-          test: /\.js$/,
-          loader: require.resolve('babel-loader'),
-          options: {
+      {
+        test: /\.js$/,
+        loader: require.resolve('babel-loader'),
+        options: {
             // This is a feature of `babel-loader` for Webpack (not Babel itself).
             // It enables caching results in ./node_modules/.cache/babel-loader/
             // directory for faster rebuilds.
-            cacheDirectory: true,
-            plugins: ['react-hot-loader/babel'],
-          },
+          cacheDirectory: true,
+          plugins: ['react-hot-loader/babel'],
         },
-        {
-          test: /\.json$/,
-          loader: 'json-loader'
-        },{
-          test: /\.s[ac]ss$/i,
-          use: extracPlugin.extract({
-            use:[
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },{
+        test: /\.s[ac]ss$/i,
+        use: extracPlugin.extract({
+          use:[
               // Translates CSS into CommonJS
-              'css-loader',
+            'css-loader',
               // Compiles Sass to CSS
-              'sass-loader',
-            ]
-          })
+            'sass-loader',
+          ]
+        })
           
-        }
+      }
     ],
   },
   plugins:[
